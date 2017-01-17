ef657d5f79a6c9c1d9e69e817d68b04a72cfbc27
Vanessa Mireles <vanessa.mireles.n@gmail.com>
2017-01-17 23:30:34 +0000

    I made a smilie face

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Write your function calls below this line
+setColor (0,0, "purple");
+setColor (0,9, "purple");
+setColor (9,0, "purple");
+setColor (9,9, "purple");
+setColor (3,2, "yellow");
+setColor (3,6, "yellow");
+setColor (4,4, "yellow");
+setColor (5,2, "yellow");
+setColor (5,6, "yellow");
+setColor (6,3, "yellow");
+setColor (6,4, "yellow");
+setColor (6,5, "yellow");
+setColor (1,8, "red");
